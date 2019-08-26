6d0f31ed1c2f2bd710cf7b039acd660fe2cd8048
Gustaw <tanski.gustaw@gmail.com>
2019-08-26 20:39:24 +0200

    built

---
+++ web/build/precache-manifest.bcb367f6f7ca1dca95cc577d442e4303.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ecb05475356371a7897b9d9447b8f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fff93613428c10cc6ca",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0fff93613428c10cc6ca",
+    "url": "/static/js/2.dc8c3e4a.chunk.js"
+  },
+  {
+    "revision": "0f5cea3a176625902b46",
+    "url": "/static/js/main.a5b32472.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
