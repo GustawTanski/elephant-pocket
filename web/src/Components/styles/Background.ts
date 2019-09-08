a0cfc3b74980abcde22b01b7e8b4d5191014dc13
deBroglie <tanski.gustaw@gmail.com>
2019-09-08 15:38:51 +0200

    typos fixed

--- web/src/Components/styles/Background.ts
+++ web/src/Components/styles/Background.ts
@@ -4,7 +4,7 @@ import { firstColor } from "../../cssVariables";
 
 export default styled.main`
 	background-color: ${firstColor};
-	min-height: 10vh;
+	min-height: 100vh;
 	/* overflow-y: scroll; */
 	overflow-x: hidden;
 	width: 100%;
